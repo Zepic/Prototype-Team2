76d3b3c3fd4aa26f31aa43d622a38e6e2adf6f92
KrzysztofGA <krzysztof@getacademy.no>
2020-06-15 11:57:17 +0200

    comments added

--- krzysztof/js/controller.js
+++ krzysztof/js/controller.js
@@ -1,27 +1,46 @@
 const contentDiv = document.getElementById('content');
 
-//testing code
+/*
+testing code
+*/
+
+//switches users to test the correctness of content display depending on the color
+//of the group and the user (possibility of agree and disagree)
 function activeUser(activeUserName) {
     model.activeUser = activeUserName;
     show();
+    //activates js to move notes after they are generated
     dragElement(document.getElementById('mydiv'));
 }
 
-//target code
+/*
+target code
+*/
+
+//shows the user page
 function clickUser(activeView) {
     model.activeView = activeView;
     show();
 }
 
+//shows the group with its notes
 function clickGroup(activeView, activeGroupName) {
     model.activeView = activeView;
     model.activeGroup = activeGroupName;
     show();
+    //activates js to move notes after they are generated
     dragElement(document.getElementById('mydiv'));
 }
 
+//shows the statistics page
 function clickStatistics(activeView, activeStatisticsName) {
     model.activeView = activeView;
     model.activeStatistics = activeStatisticsName;
     show();
 }
+
+/* 
+QUESTIONS:
+1. How to use arrow function in onclick (for clickNavigation() )
+2. 
+*/
