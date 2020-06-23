268e0a645ae3a33010aebe3fa34f3c0e9979764b
audunaell <62010741+audunaell@users.noreply.github.com>
2020-06-23 14:18:44 +0200

    testing with firebase

---
+++ theMadnessofAndreas/Andreasfirebasetesting/model.js
@@ -0,0 +1,50 @@
+//model
+const model = {
+    user: {
+        name: 'Terje',
+        color: 'green',
+    },
+    activeView: 'startPage',
+    activeUser: '',
+    activeGroup: 'blue 1',
+    activeAboutColor: 'blue',
+    groups: [
+        {
+            name: 'red 1',
+            color: 'red',
+        },
+        {
+            name: 'green 1',
+            color: 'green',
+        },
+        {
+            name: 'blue 1',
+            color: 'blue',
+        },
+        {
+            name: 'red 2',
+            color: 'red',
+        },
+        {
+            name: 'green 2',
+            color: 'green',
+        },
+        {
+            name: 'blue 2',
+            color: 'blue',
+        },
+        {
+            name: 'red 3',
+            color: 'red',
+        },
+        {
+            name: 'green 3',
+            color: 'green',
+        },
+        {
+            name: 'blue 3',
+            color: 'blue',
+        },
+    ],
+    updates: [], //insert fake news
+};
